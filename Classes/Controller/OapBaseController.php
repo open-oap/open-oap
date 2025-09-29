0514e4d655a3383a10ed14c7744af950a6bb5b30
Thorsten Born <thorsten.born@cosmoblonde.de>
2025-09-29 17:03:37 +0200

    Configures default items per page for pagination
    Moves the pagination items per page configuration to a more suitable location and sets it to a default value.
    
    This ensures a consistent number of items displayed per page in paginated views.

--- Classes/Controller/OapBaseController.php
+++ Classes/Controller/OapBaseController.php
@@ -309,7 +309,9 @@ class OapBaseController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControll
      */
     protected const TEXTAREA_MEDIUM_MIN_CHARS = 300;
     protected const TEXTAREA_MEDIUM_MAX_CHARS = 1500;
-    
+
+    protected const PAGINATOR_ITEMS_PER_PAGE = 50;
+
     protected const SURVEY_URL_PARAMETER_CALLID = 'survey';
     protected const SURVEY_URL_PARAMETER_HASH = 'hash';
 
@@ -318,7 +320,6 @@ class OapBaseController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControll
     // PAGINATOR VALUES
     protected const PAGINATOR_PAGES = 1;
     protected const PAGINATOR_ITEMS = 2;
-    protected const PAGINATOR_ITEMS_PER_PAGE = 1000;
     protected const PAGINATOR_SHOW_AFTER = 10;
     protected const PAGINATOR_SHOW_BEFORE = 10;
 
